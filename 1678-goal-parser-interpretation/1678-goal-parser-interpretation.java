aa1512ebc77f99a3cf93a533762b313fc9ae2ea0
avi28ral <aviralsrivastava2802@gmail.com>
2026-07-13 19:35:29 +0530

    Time: 1 ms (75.39%), Space: 42.9 MB (52.24%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String interpret(String command) {
+        StringBuilder sb = new StringBuilder();
+        int i =0;
+        while(i < command.length()){
+            if(command.charAt(i) == 'G'){
+                sb.append("G");
+                i++;
+            }else if(command.charAt(i) == '('){
+                i++;
+            }else if(command.charAt(i) ==')'){
+                sb.append("o");
+                i++;
+            }else{
+                sb.append("al");
+                i += 3;
+            }
+        }
+        return sb.toString();
+
+    }
+}
